31dc6b0033872fd5172d56c1ad1851aeefa42513
AndreaDex <desiderio.andrea@outlook.it>
2021-06-28 15:45:09 +0200

    crete class Person and Customer

--- index.php
+++ index.php
@@ -26,15 +26,49 @@ class Person
 
 }
 
+/**
+ * Estende la classe Person
+ */
 class Customer extends Person 
 {
+    protected $id;
+    protected $password;
     protected $address;
     protected $credit_card;
+    protected $cart;
+    protected $premium = false;
 
-    function setAddress($newAddress){
-        $this->address = $newAddress
+    public function getPremium(){
+        return $this->premium;
     }
 
-    
+    private function setPassword($newPassword){
+        $this->password = $newPassword;        
+    }
+    public function setAddress($newAddress){
+        $this->address = $newAddress;
+    }
+
+    protected function setCardNumber(int $cardNumber){
+        $this->credit_card = $cardNumber;
+    }
+
+    function  __construct($name, $lastname, $email, $phone, $id,){
+        parent::__construct($name, $lastname, $email, $phone);
+        $this->id = $id;
+    }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHOP CLASS</title>
+</head>
+<body>
+    
+</body>
+</html>
